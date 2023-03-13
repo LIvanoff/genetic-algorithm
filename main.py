eb6bf97a32cbe4de7ed7f0783763b797fe70a205
LIvanoff <drfrily@gmail.com>
2023-03-14 01:24:50 +0300

    update main.py

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ import pandas as pd
 
 if __name__ == "__main__":
     df_train = pd.read_excel('test.xlsx')
-    population = GeneticAlgorithm(10, 0, 0, df_train['square'].to_numpy(), df_train['clusters'].to_numpy())
-
-    # plt.scatter(df_train['square'], df_train['clusters'], marker='o', alpha=0.8)
-    # plt.plot(pop.individuals[:, 0], pop.individuals[:, 1])
+    population = GeneticAlgorithm(100, 0, 0, df_train['square'].to_numpy(), df_train['clusters'].to_numpy())
     population.select()
-    # plt.show()
