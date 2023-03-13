d31185a22fc35c8132ea941666b1dc9b2a183eb8
LIvanoff <drfrily@gmail.com>
2023-03-13 21:25:18 +0300

    update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,6 +1,7 @@
 from population import Population
 
 
-class Algorithm(Population):
+class GeneticAlgorithm(Population):
     def __init__(self, population_size: int, b0: int, b1: int):
         super().__init__(population_size, b0, b1)
+
