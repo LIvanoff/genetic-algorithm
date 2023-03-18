7f0aa477377070c9bd70efdccebc7e06c1b1a4ad
LIvanoff <drfrily@gmail.com>
2023-03-18 14:01:44 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from genetic_algorithm import GeneticAlgorithm
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
 if __name__ == "__main__":
     df_train = pd.read_excel('test.xlsx')
+    X = df_train['square'].to_numpy()
+    Y = df_train['clusters'].to_numpy()
+
     population = GeneticAlgorithm(
         population_size=100,
         b0=0,
         b1=0,
         sigma_b0=0.1,
         sigma_b1=1000,
-        train=df_train['square'].to_numpy(),
-        target=df_train['clusters'].to_numpy())
+        train=X,
+        target=Y)
     population.select()
+
+    x_loss = np.arange(len(population.loss_history))
+    print(len(x_loss))
+    print(len(population.loss_history))
+    plt.plot(x_loss, population.loss_history)
+    plt.show()
