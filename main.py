c79baa446c81e39c74aebc4eed5d068435100d9d
Leonid <drfrily@gmail.com>
2023-03-18 13:31:45 +0300

    update main.py

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ import pandas as pd
 
 if __name__ == "__main__":
     df_train = pd.read_excel('test.xlsx')
-    population = GeneticAlgorithm(100, 0, 0, df_train['square'].to_numpy(), df_train['clusters'].to_numpy())
+    population = GeneticAlgorithm(
+        population_size=100,
+        b0=0,
+        b1=0,
+        sigma_b0=0.1,
+        sigma_b1=1000,
+        train=df_train['square'].to_numpy(),
+        target=df_train['clusters'].to_numpy())
     population.select()
